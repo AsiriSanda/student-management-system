b93cd765d6f7a960b93f336f77b9d9fd90fb0cc7
AsiriSanda <sadakelum1234@gmail.com>
2022-07-12 12:59:01 +0530

    completed teacher

--- src/controller/TeacherSaveFormController.java
+++ src/controller/TeacherSaveFormController.java
@@ -2,8 +2,10 @@ package controller;
 
 import db.DbConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import model.Student;
+import model.Teacher;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,20 +18,32 @@ public class TeacherSaveFormController {
     public TextField txtNic;
     public TextField txtContact;
 
-    public void SaveTeacherOnAction(ActionEvent actionEvent) {
+    public void SaveTeacherOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Teacher t1 = new Teacher(
+                txtId.getText(),txtName.getText(),
+                txtAddress.getText(),txtNic.getText(),txtContact.getText()
+        );
+
+
+        if(saveTeacher(t1))
+            new Alert(Alert.AlertType.CONFIRMATION, "Saved..").show();
+        else
+            new Alert(Alert.AlertType.WARNING, "Try Again..").show();
+
+
+
 
     }
 
     boolean saveTeacher(Teacher t) throws SQLException, ClassNotFoundException {
         Connection con= DbConnection.getInstance().getConnection();
-        String query="INSERT INTO Student VALUES(?,?,?,?,?,?)";
+        String query="INSERT INTO Teacher VALUES(?,?,?,?,?)";
         PreparedStatement stm = con.prepareStatement(query);
-        stm.setObject(1,s.getStudent_id());
-        stm.setObject(2,s.getStudent_name());
-        stm.setObject(3,s.getEmail());
-        stm.setObject(4,s.getContact());
-        stm.setObject(5,s.getAddress());
-        stm.setObject(6,s.getNic());
+        stm.setObject(1,t.getTeacher_id());
+        stm.setObject(2,t.getName());
+        stm.setObject(3,t.getNic());
+        stm.setObject(4,t.getContact());
+        stm.setObject(5,t.getAddress());
 
 
         return stm.executeUpdate()>0;
